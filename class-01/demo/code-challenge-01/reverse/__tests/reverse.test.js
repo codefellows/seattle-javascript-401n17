c55a8b30383bd695676fa4206820213dd7ffd9d1
John Cokos <highlander.44@gmail.com>
2020-08-10 20:31:32 -0700

    Added Code Challenge 01 Solution

---
+++ class-01/demo/code-challenge-01/reverse/__tests/reverse.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let reverseArray = require('../reverse.js');
+
+// reverseArray is a function
+// reverseArray is the function exported from reverse.js
+
+let myNumbers = [2, 4, 6, 8, 10];
+reverseArray(myNumbers);
+
+console.log(myNumbers);
