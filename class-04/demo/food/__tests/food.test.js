83095eaa9d469da59fb0c3d1b9326eab95cc5bd4
John Cokos <highlander.44@gmail.com>
2020-08-20 12:58:06 -0700

    Demo updated

--- class-04/demo/food/__tests/food.test.js
+++ class-04/demo/food/__tests/food.test.js
@@ -3,33 +3,42 @@
 require('@code-fellows/supergoose');
 
 const Food = require('../models/food-collection.js');
+
 const food = new Food();
 
-describe('Food Model', () => {
+describe('Food Collection', () => {
+
   it('can .post() a new record', () => {
 
-    const item = {
-      name: 'test food 1',
-      calories: 10,
-      type: "meat"
+    let obj = {
+      name: 'test record',
+      calories: 1,
+      type: 'meat'
     };
 
-    return food.post(item)
+    return food.post(obj)
       .then(record => {
         expect(record._id).toBeDefined();
       })
 
-  })
+  });
 
-  it('can .get() a record', async () => {
-    const item = {
-      name: 'test food 2',
-      calories: 10,
-      type: "meat"
+  it('can .get() a record from the database', async () => {
+    // insert a new record
+    // use it's _id
+    // find that record (with the _id that we just added)
+    // Assert that we have data
+
+    let obj = {
+      name: 'test record',
+      calories: 1,
+      type: 'meat'
     };
 
-    const record = await food.post(item)
-    const found = await food.get(record.id)
-    expect(found._id).toEqual(record._id);
-  })
-})
+    let insertedRecord = await food.post(obj);
+    let foundRecord = await (food.get(insertedRecord._id))
+    expect(foundRecord._id).toEqual(insertedRecord._id);
+
+  });
+
+});
