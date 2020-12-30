527becf4e72c785927fbc9291af6d220d9a7892c
John Cokos <highlander.44@gmail.com>
2020-12-29 20:55:11 -0800

    RNA Demo

--- class-37/combined-reducers/src/store/candidates-reducer.js
+++ class-37/combined-reducers/src/store/candidates-reducer.js
@@ -1,31 +1,32 @@
 let initialState = [
-  {name: "John", votes:0 },
-  {name: "Cathy", votes:0 },
-  {name: "Zach", votes:0 },
-  {name: "Allie", votes:0 },
+  {name: 'John', votes:0 },
+  {name: 'Cathy', votes:0 },
+  {name: 'Zach', votes:0 },
+  {name: 'Allie', votes:0 },
 ];
 
 // Reducer function takes in the current state and returns the next state
 export default function reducer(state=initialState, action) {
+  console.log('reducer fired', state);
   // let {type, payload} = action;
 
   switch( action.type ) {
-    case 'VOTE':
-      // get a candiate object and need to up the vote count
-      // action.payload is the candidate
-      return state.map( candidate => {
-        if(candidate.name === action.payload.candidate.name) {
-          return { name: candidate.name, votes: candidate.votes + 1 }
-        } else {
-          return candidate;
-        }
-      });
-      // Ugly/Beautiful(spread) way ...
-      // return state.map( candidate => candidate.name === action.payload.name ? {...candidate, votes: candidate.votes + 1} : candidate )
-    case 'RESET':
-      return initialState;
-    default:
-      return state;
+  case 'VOTE':
+    // get a candiate object and need to up the vote count
+    // action.payload is the candidate
+    return state.map( candidate => {
+      if(candidate.name === action.payload.candidate.name) {
+        return { name: candidate.name, votes: candidate.votes + 1 };
+      } else {
+        return candidate;
+      }
+    });
+    // Ugly/Beautiful(spread) way ...
+    // return state.map( candidate => candidate.name === action.payload.name ? {...candidate, votes: candidate.votes + 1} : candidate )
+  case 'RESET':
+    return initialState;
+  default:
+    return state;
   }
 }
 
@@ -35,13 +36,13 @@ export const castVote = (person, voter) => {
     type: 'VOTE',
     payload: {
       candidate: person,
-      voter: voter
-    }
-  }
-}
+      voter: voter,
+    },
+  };
+};
 
 export const reset = () => {
   return {
     type: 'RESET',
-  }
-}
+  };
+};
